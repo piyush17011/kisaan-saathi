cdb0fae85b53c47ed9754d4b05c0d6fe894d757f
piyush17011 <piyushbhalwalkar01@gmail.com>
2026-05-02 14:53:41 +0530

    deleted test gemini py as it was leaking my api kety

--- test_gemini.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBtX9SYp2eG0TLj1m_e2n9HgVNGRWDPDKY")
-
-model = genai.GenerativeModel("gemini-2.5-flash-lite")
-
-response = model.generate_content("Say hello")
-
-print(response.text)
